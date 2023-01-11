40972f1e280c5b0a9f8e70cd07155c736fffe5d1
A-v-i-n-a-s-h-p-a-n-d-e-y <114899346+A-v-i-n-a-s-h-p-a-n-d-e-y@users.noreply.github.com>
2023-01-10 19:08:31 -0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import react from 'react'
+
+const Footer = () => {
+    return <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https;//youtube.com" target={"blank"}>Youtube</a>
+                <a href="https;//Instagram.com" target={"blank"}>Instagram</a>
+                <a href="https;//Github.com" target={"blank"}>Github</a>
+
+            </div>
+        </div>
+
+    </footer>
+        
+    
+}
+export default Footer
+
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+   AiFillGoogleCircle,
+   AiFillAmazonCircle,
+   AiFillYoutube,
+   AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return(
+     <>
+     <div className="home" id="home" >
+        <main>
+          <h1>TechyStar</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+      
+      <div className="home2">
+     <img src={vg} alt="Graphics" />
+     
+     <div>
+        <p>
+        We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+        </p>
+     </div>
+     </div>
+
+     <div className="home3" id="about">
+        <div>
+            <h1>Who we are?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Provident voluptatibus maxime ipsum error perferendis laudantium tenetur excepturi fugiat corrupti odio voluptate fugit facilis, exercitationem saepe assumenda commodi ab, nemo perspiciatis quis.
+                 Sapiente illum ipsum officia, odit aut eligendi tenetur, dolor quaerat perspiciatis quo natus cupiditate
+                  voluptatum omnis provident itaque labore!</p>
+        </div>
+     </div>
+
+     <div className="home4" id="brands">
+      <div>
+         <h1>Brands</h1>
+         <article>
+            <div 
+            style={{
+               animationDelay:"0.3s",
+            }}
+            >
+               <AiFillGoogleCircle/>
+               <p>Google</p>
+            </div>
+
+            <div 
+
+            style={{
+               animationDelay:"0.5s",
+            }}
+            >
+               <AiFillAmazonCircle/>
+               <p>Amazon</p>
+            </div>
+
+            <div 
+            style={{
+               animationDelay:"0.7s",
+            }}
+            >
+               <AiFillYoutube/>
+               <p>Youtube</p>
+            </div>
+
+            <div 
+            style={{
+               animationDelay:"1s",
+            }}
+            >
+               <AiFillInstagram/>
+               <p>Instagram</p>
+            </div>
+         </article>
+
+      </div>
+      </div>
+
+     </>
+  );
+  
+};
+
+export default Home;
